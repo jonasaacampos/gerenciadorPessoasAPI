d8d52ef91f2241f7e924ca90099e949292dd9254
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-01-25 11:53:51 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+## Sistema de gerenciamento de pessoas em API com Spring Boot
+
+Cognizant|Java Developer|2022
+
+http://localhost:8080/api/v1/people
+Pré-requisitos para a execução do projeto desenvolvido:
+
+- Conta no GitHub para o armazenamento do seu projeto na nuvem.
+- Conta no Heroku para o deploy do projeto
+
---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,57 @@
+package one.digitalinnovation.personapi.controller;
+
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/people")
+
+
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class PersonController {
+
+    //Restfull personapi desenvolvida com as boas práticas de programação.
+    private PersonService personService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+       public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.create(personDTO);
+
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+       return  personService.listAll();
+
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+    return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO update(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.update(id, personDTO);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+}
